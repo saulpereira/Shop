27dc549d7c2e5daeda5f8cec9697cf923e6a1bd2
Saul Pereira <saul_pereira@hotmail.com>
2021-04-23 15:19:48 -0600

    Repository pattern implemented

---
+++ Shop/Shop.Web/Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Shop.Web.Data
+{
+    using Shop.Web.Data.Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    public interface IRepository
+    {
+        void AddProduct(Product product);
+        Product GetProduct(int id);
+        IEnumerable<Product> GetProducts();
+        bool ProductExists(int id);
+        void RemoveProduct(Product product);
+        Task<bool> SaveAllAsync();
+        void UpdateProduct(Product product);
+    }
+}
--- Shop/Shop.Web/Program.cs
+++ Shop/Shop.Web/Program.cs
@@ -1,16 +1,43 @@
-using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;*/
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Web.Data;
 
 namespace Shop.Web
 {
     public class Program
     {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
+        }
+
+        private static void RunSeeding(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+
+        /*
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -21,6 +48,6 @@ namespace Shop.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }); */
     }
 }
